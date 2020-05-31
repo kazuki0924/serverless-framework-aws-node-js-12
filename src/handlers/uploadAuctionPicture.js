4e7d811f74458c00347729e9d6493d2381ef6371
Kazuki Matsuzaki <kazuki0924@gmail.com>
2020-06-01 00:03:48 +0900

    25

--- src/handlers/uploadAuctionPicture.js
+++ src/handlers/uploadAuctionPicture.js
@@ -1,11 +1,20 @@
 /** @format */
 import { getAuctionById } from './getAuction';
+import { uploadPictureToS3 } from '../lib/uploadPictureToS3';
 
 export async function uploadAuctionPicture(event) {
 	const { id } = event.pathParameters;
 	const auction = await getAuctionById(id);
 	const base64 = event.body.replace(/^data:image\/\w+;base64,/, '');
 	const buffer = Buffer.from(base64, 'base64');
+
+	const uploadToS3Result = await uploadPictureToS3(
+		auction.id + '.jpeg',
+		buffer
+	);
+
+	console.log(uploadToS3Result);
+
 	return {
 		statusCode: 200,
 		body: JSON.stringify({}),
--- src/lib/uploadPictureToS3.js
+++ src/lib/uploadPictureToS3.js
@@ -1,11 +1,18 @@
 /** @format */
 
-import { Lambda } from 'aws-sdk';
-
 import AWS from 'aws-sdk';
 
 const s3 = new AWS.S3({});
 
 export async function uploadPictureToS3(key, body) {
-	await s3.upload({}).promise();
+	const result = await s3
+		.upload({
+			Bucket: process.env.AUCTIONS_BUCKET_NAME,
+			Key: key,
+			Body: body,
+			ContentEncoding: 'base64',
+			ContentType: 'image/jpeg',
+		})
+		.promise();
+	return result;
 }
