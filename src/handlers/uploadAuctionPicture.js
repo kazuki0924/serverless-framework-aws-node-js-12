51a334f7b7819d57572351b0dab2e25056408a16
Kazuki Matsuzaki <kazuki0924@gmail.com>
2020-06-03 09:35:10 +0900

    31

--- src/handlers/uploadAuctionPicture.js
+++ src/handlers/uploadAuctionPicture.js
@@ -1,33 +1,43 @@
-/** @format */
-
 import middy from '@middy/core';
 import httpErrorHandler from '@middy/http-error-handler';
+import validator from '@middy/validator';
+import cors from '@middy/http-cors';
 import createError from 'http-errors';
 import { getAuctionById } from './getAuction';
 import { uploadPictureToS3 } from '../lib/uploadPictureToS3';
-import setAuctionPictureUrl from '../lib/setAuctionPictureUrl';
+import { setAuctionPictureUrl } from '../lib/setAuctionPictureUrl';
+import uploadAuctionPictureSchmea from '../lib/schemas/uploadAuctionPictureSchema';
 
 export async function uploadAuctionPicture(event) {
-	const { id } = event.pathParameters;
-	const auction = await getAuctionById(id);
-	const base64 = event.body.replace(/^data:image\/\w+;base64,/, '');
-	const buffer = Buffer.from(base64, 'base64');
+  const { id } = event.pathParameters;
+  const { email } = event.requestContext.authorizer;
+  const auction = await getAuctionById(id);
 
-	let updatedAuction;
+  // Validate auction ownership
+  if (auction.seller !== email) {
+    throw new createError.Forbidden(`You are not the seller of this auction!`);
+  }
 
-	try {
-		const pictureUrl = await uploadPictureToS3(auction.id + '.jpg', buffer);
-		console.log(pictureUrl);
-		updatedAuction = await setAuctionPictureUrl(auction.id, pictureUrl);
-	} catch (error) {
-		console.error(error);
-		throw new createError.InternalServerError(error);
-	}
+  const base64 = event.body.replace(/^data:image\/\w+;base64,/, '');
+  const buffer = Buffer.from(base64, 'base64');
 
-	return {
-		statusCode: 200,
-		body: JSON.stringify({ updatedAuction }),
-	};
+  let updatedAuction;
+
+  try {
+    const pictureUrl = await uploadPictureToS3(auction.id + '.jpg', buffer);
+    updatedAuction = await setAuctionPictureUrl(auction.id, pictureUrl);
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(updatedAuction),
+  };
 }
 
-export const handler = middy(uploadAuctionPicture).use(httpErrorHandler());
+export const handler = middy(uploadAuctionPicture)
+  .use(httpErrorHandler())
+  .use(validator({ inputSchema: uploadAuctionPictureSchmea }))
+  .use(cors());
